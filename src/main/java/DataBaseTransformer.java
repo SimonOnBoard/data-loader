cdf7455f995ae651a2426cf965f095a09df9611a
Simon Konkov <sweetsixteen.sudo@gmail.com>
2020-03-18 00:30:59 +0300

    commit jar

--- src/main/java/DataBaseTransformer.java
+++ src/main/java/DataBaseTransformer.java
@@ -1,5 +1,6 @@
 import java.applet.AppletStub;
 import java.io.UnsupportedEncodingException;
+import java.lang.invoke.SwitchPoint;
 import java.security.SecureRandom;
 import java.sql.*;
 import java.time.LocalDateTime;
@@ -22,49 +23,64 @@ public class DataBaseTransformer {
             LocalDateTime localDateTime = LocalDateTime.now();
             int day = localDateTime.getDayOfMonth();
             int month = localDateTime.getMonth().getValue();
-            Map<Integer,String> months = new HashMap<>();
-            months.put(1,"Jan");
-            months.put(2,"Feb");
-            months.put(3,"Mar");
-            months.put(4,"Apr");
-            months.put(5,"May");
-            months.put(6,"Jun");
-            months.put(7,"Jul");
-            months.put(8,"Aug");
-            months.put(9,"Sep");
-            months.put(10,"Oct");
-            months.put(11,"Now");
-            months.put(12,"Dec");
+            Map<Integer, String> months = new HashMap<>();
+            months.put(1, "Jan");
+            months.put(2, "Feb");
+            months.put(3, "Mar");
+            months.put(4, "Apr");
+            months.put(5, "May");
+            months.put(6, "Jun");
+            months.put(7, "Jul");
+            months.put(8, "Aug");
+            months.put(9, "Sep");
+            months.put(10, "Oct");
+            months.put(11, "Now");
+            months.put(12, "Dec");
             String sql = "SELECT text from tweets_2 where day = ? and month = ? order by tweet_id DESC";
 
             PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1,"" + day);
-            statement.setString(2,months.get(month));
+            statement.setString(1, "" + day);
+            statement.setString(2, months.get(month));
             ResultSet resultSet = statement.executeQuery();
             Integer i = 0;
+            List<String> strings;
             while (resultSet.next()) {
+                strings = new ArrayList<>();
                 String text = resultSet.getString("text");
-                String[] strings = text.split(" ");
-                base.put(i, strings);
+                String[] words = text.split("\\s*(\\s|,|!|\\.)\\s*");
+                StringBuilder sb = new StringBuilder();
+                for (int k = 0; k < words.length; k++) {
+                    sb.setLength(0);
+                    words[k] = words[k].toLowerCase();
+                    for (int j = 0; j < words[k].length(); j++) {
+                        if (Character.isAlphabetic(words[k].charAt(j))) {
+                            sb.append(words[k].charAt(j));
+                        }
+                    }
+                    if(sb.length() != 0) {
+                        strings.add(sb.toString());
+                    }
+                }
+                base.put(i, strings.toArray(new String[strings.size()]));
                 i++;
             }
             processData();
             String name = "tweets_last_version";
             //language=sql
-            String monthDay = localDateTime.getMonth().getValue()+ "_" + localDateTime.getDayOfMonth();
+            String monthDay = localDateTime.getMonth().getValue() + "_" + (localDateTime.getDayOfMonth());
             sql = "CREATE TABLE IF NOT EXISTS " + name + " (word varchar, count int, month_day varchar);";
             System.out.println(sql);
             statement = connection.prepareStatement(sql);
             statement.executeUpdate();
             PreparedStatement preparedStatement = connection.prepareStatement("INSERT into " + name + " VALUES (?,?,?)");
-            preparedStatement.setString(3,monthDay);
+            preparedStatement.setString(3, monthDay);
             String word;
             for (String s : dictionary.keySet()) {
                 word = new String(s.getBytes(), "UTF-8");
                 preparedStatement.setString(1, word);
                 preparedStatement.setInt(2, dictionary.get(s));
                 int a = preparedStatement.executeUpdate();
-                if(a == 0){
+                if (a == 0) {
                     throw new IllegalStateException("Database is unavaliable");
                 }
             }
